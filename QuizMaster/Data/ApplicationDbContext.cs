0ed7e8fb199bd7c1b071a63082424ea79211f369
Monyechi <63468925+Monyechi@users.noreply.github.com>
2020-06-17 00:24:52 -0400

    initialCommit

---
+++ QuizMaster/Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using QuizMaster.Models;
+
+namespace QuizMaster.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<IdentityRole>()
+            .HasData(
+            new IdentityRole
+            {
+                Name = "Instructor",
+                NormalizedName = "INSTRUCTOR"
+            },
+             new IdentityRole
+            {
+                Name = "Student",
+                NormalizedName = "STUDENT"
+            }
+            );
+        }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ QuizMaster/Models/KeyAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace QuizMaster.Models
+{
+    internal class KeyAttribute : Attribute
+    {
+    }
+}
